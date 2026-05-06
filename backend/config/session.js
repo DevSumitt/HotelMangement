9125ee19c29da2d4093c51b060ecbd082f599e2c
Sumit <suumroiy@gmail.com>
2026-05-06 16:55:01 +0530

    Update session.js

--- backend/config/session.js
+++ backend/config/session.js
@@ -1,22 +1,20 @@
 const session = require("express-session");
-const MongoStore = require("connect-mongo").default;
-
+const MongoStore = require("connect-mongo"); 
 
 module.exports = session({
     name: 'login-session',
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({
+    proxy: true,
+    store: MongoStore.create({ 
         mongoUrl: process.env.MONGODB_URI,
         collectionName: "sessions"
     }),
-
-
     cookie: {
         httpOnly: true,
         maxAge: 1000 * 60 * 60,
         secure: true,
-        sameSite: "lax"
+        sameSite: "none" 
     }
-})
+});
