77f4de7584f47031a19e7b6d0ceeb7cd251f7470
Sumit <suumroiy@gmail.com>
2026-05-06 12:34:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app = express()
 app.use(bodyParse.json());
 myDB();
 app.use(cors({
-    origin: "https://hotel-mangement-f8nr.vercel.app/",
+    origin: "https://hotel-mangement-2krz.vercel.app",
     credentials: true
 }))
 app.get("/test",(req,res)=>{
